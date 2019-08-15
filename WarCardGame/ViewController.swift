0fcc10782d89b1aed696873a384553826c3b8914
Denver Abelarde <denver@nexusgaming.tv>
2019-08-15 10:28:52 +0800

    Initial commit > UI Setup > Custom layout for lower screen resolution.

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Denver Abelarde on 11/08/2019.
+//  Copyright © 2019 Denver IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
